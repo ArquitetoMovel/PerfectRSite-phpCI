c34f5b21f37288eb98600ea7a7debbcea4d8e81c
Alexandre Danelon <alexandredanelon@MacBook-Pro-de-Alexandre.local>
2017-04-30 19:17:32 -0300

    POC do Artigo Asp.net vs PHP - Site PHP

---
+++ application/views/services.php
@@ -0,0 +1,168 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!-- ==============================================
+		SERVICES
+		=============================================== -->
+		<div id="services" class="bg-light pt-120">
+		
+			<div class="container">
+			
+				<div class="scrolling-animation fade-up">
+					<h2 class="text-lg text-center">See how I can <span class="text-color">help you</span></h2>
+					<hr class="hr-gray hr-center hr-sm hr-bold mb-20"/>
+					<p class="text-gray text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum vehicula condimentum.</p>
+				</div>
+				
+				<div class="row mt-160">
+				
+					<div class="col-lg-5 text-center mt-40 mb-40 scrolling-animation fade-left">
+					
+						<h3 class="text-lg text-bold mb-10">Responsive Design</h3>
+						<h3 class="text-gray mb-40 text-color text-bold">Why</h3>
+						
+						<p class="text-lg">
+							<sup><i class="fa fa-quote-left fa-fw text-gray"></i></sup>
+							Day by day, the number of devices, platforms, and browsers that need to work with your site grows. Responsive web design represents a fundamental shift in how we’ll build websites for the decade to come.
+							<sup><i class="fa fa-quote-right fa-fw text-gray"></i></sup>
+						</p>
+						
+						<p class="text-gray text-right"><em>- Jeffrey Veen</em></p>
+					
+					</div>
+				
+					<div class="col-lg-7 scrolling-animation fade-left d1">
+					
+						<div class="multiple-devices synchronized-sliders">
+						
+							<div class="macbook-mockup single-device">
+							
+								<img class="img-responsive img-center" src="assets/images/macbook-single.png" alt=""/>
+								
+								<div class="macbook-mockup-content flexslider no-direction-nav no-bullet-nav effect-zoom" data-speed="2500">
+								  <ul class="slides">
+									<li>
+									  <img src="assets/images/macbook-screen-1.jpg" alt="" />
+									</li>
+									<li>
+									  <img src="assets/images/macbook-screen-2.jpg" alt="" />
+									</li>
+									<li>
+									  <img src="assets/images/macbook-screen-3.jpg" alt="" />
+									</li>
+								  </ul>
+								</div>
+								<a href="#desktop-modal" data-toggle="modal"><span style="top:-5%;left:10%;" class="poi">Click Me</span></a>
+							</div>
+							
+							
+							<div class="iphone-mockup single-device">
+							
+								<img class="img-responsive img-center" src="assets/images/iphone-single.png" alt=""/>
+								
+								<div class="iphone-mockup-content flexslider pause no-direction-nav no-bullet-nav effect-slide" data-speed="2500">
+									<ul class="slides">
+									<li>
+									  <img src="assets/images/iphone-screen-1.jpg" alt="" />
+									</li>
+									<li>
+									  <img src="assets/images/iphone-screen-2.jpg" alt="" />
+									</li>
+									<li>
+									  <img src="assets/images/iphone-screen-3.jpg" alt="" />
+									</li>
+									</ul>
+								</div>
+								<a href="#mobile-modal" data-toggle="modal"><span style="top:-8%;right:-15%;" class="poi delay-animation">Click Me</span></a>
+							</div>
+								
+							
+						</div>
+					
+					</div>
+				
+				</div>
+			
+				<hr class="mt-80 mb-80"/>
+			
+				<div class="pb-80 hidden-xs"></div>
+			
+				<div class="row">
+				
+					<div class="col-lg-5 col-lg-offset-1 col-lg-push-6 text-center mt-100 mb-40  scrolling-animation fade-right">
+					
+						<h3 class="text-lg text-bold mb-10">Creative UX / UI</h3>
+						<h3 class="text-gray mb-40 text-color text-bold">What</h3>
+						
+						<p class="text-lg">
+							<sup><i class="fa fa-quote-left fa-fw text-gray"></i></sup>
+							A great product experience starts with UX followed by UI. Both are essential for the product’s success.
+							<sup><i class="fa fa-quote-right fa-fw text-gray"></i></sup>
+						</p>
+						
+						<p class="text-gray text-right"><em>- Rahul Varshney</em></p>
+					
+					</div>
+				
+					<div class="col-lg-6 col-lg-pull-6 scrolling-animation fade-right d1">
+					
+					
+						<div class="imac-mockup single-device">
+						
+							<img class="img-responsive" src="assets/images/imac-single.png" alt=""/>
+							
+							<div class="imac-mockup-content">
+							
+								<div class="twentytwenty-container">
+									<!-- The before image is first -->
+									<img class="img-responsive" src="assets/images/code.png" alt="" />
+									<!-- The after image is last -->
+									<img class="img-responsive" src="assets/images/mac-thumb.jpg" alt="" />
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+			</div>
+		
+		</div>
+		
+		<div class="bg-gray pt-80 pb-80">
+		
+			<div class="container">
+			
+				<div class="row">
+				
+					<div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 mb-40 scrolling-animation fade-left">
+					
+						<h3 class="text-hover-color icon-hover-zoom-in-all"><span class="icon-inline icon-circle icon-sm icon-solid-color nml"><i class="fa fa-wordpress"></i></span>Wordpress</h3>
+					
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum vehicula condimentum. Praesent iaculis mauris dolor, eu eleifend orci mollis quis.</p>
+					
+					</div>
+				
+					<div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 mb-40 scrolling-animation fade-left d1">
+					
+						<h3 class="text-hover-color icon-hover-zoom-in-all"><span class="icon-inline icon-circle icon-sm icon-solid-color nml"><i class="fa fa-joomla"></i></span>Joomla</h3>
+					
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum vehicula condimentum. Praesent iaculis mauris dolor, eu eleifend orci mollis quis.</p>
+					
+					</div>
+				
+					<div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 mb-40 scrolling-animation fade-left d2">
+					
+						<h3 class="text-hover-color icon-hover-zoom-in-all"><span class="icon-inline icon-circle icon-sm icon-solid-color nml"><i class="fa fa-drupal"></i></span>Drupal</h3>
+					
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum vehicula condimentum. Praesent iaculis mauris dolor, eu eleifend orci mollis quis.</p>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
